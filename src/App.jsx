f67148d51949243bfb048737e34ef0f5738947cd
Rafael Lana <134884057+RafaelLanaDS@users.noreply.github.com>
2026-09-06 14:46:58 -0300

    Primeiras alteraçoes no NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import './App.css'
 import { useState } from 'react'
 
+import {motion} from 'framer-motion'
+
+import {ArrowRight, Check, Menu, X, Star, Shield, Zap, Sparkles} from 'lucide-react'
+
+const navLinks = [
+  {href: "#features", label: "Features"},
+  {href: "#testimonials", label: "Testimonials"},
+  {href: "#pricing", label: "Pricing"},
+  {href: "#faq", label: "FAQ"},
+  // esses são os links que vão aparecer no menu de navegação
+]
+
 function App() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   
   return (
-    <>
-      <h1 className='text-3xl font-bold'>Hello word React Tailwind</h1>
-    </>
+    <div className="bg-slate-950 text-slate-100 selection:bg-fushia-500/30">
+      {/* NavBar */}
+      <header className="sticky top-0 z-40 border-b border-white/5">
+        <div>
+
+          <a href="#" className="flex items-center gap-2 ">
+            <Sparkles className="size-5 text-fuchsia-500" />
+            <span className="font-bold  tracking-tight">Minha marca</span>
+          </a>
+
+          <nav>
+            <ul>
+              {navLinks.map((link) => (
+                <li key={link.href}>
+                  <a href={link.href}>{link.label}</a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+        </div>
+      </header>
+    </div>
   )
 }
 
