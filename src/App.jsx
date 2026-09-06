273226e7e45c633b9a55106a63cc8b2c90bd5591
Rafael Lana <134884057+RafaelLanaDS@users.noreply.github.com>
2026-09-06 14:13:41 -0300

    Criar projeto React inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-3xl font-bold'>Hello word React Tailwind</h1>
+    </>
+  )
+}
+
+export default App
