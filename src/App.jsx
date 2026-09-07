c18ca6f5e61d7d9a28b87165602882245f11ad03
Rafael Lana <134884057+RafaelLanaDS@users.noreply.github.com>
2026-09-07 16:09:14 -0300

    finalizando a barra de navageção mobile

--- src/App.jsx
+++ src/App.jsx
@@ -42,11 +42,32 @@ function App() {
           </button>
 
         </div>
+
         {open && (
           <div className='md:hidden'>
-            <p>menu</p>
+            <div className='fixed bg-black/60' onClick={() => setOpen(false)}>
+                <div className='fixed right-0 top-0 h-full w-80 bg-slate-900 border-l border-white/10 p-6'>
+                  <div className='flex items-center justify-between mb-2'>
+                    <div className='flex items-center gap-2'>
+                      <Sparkles className='size-6 text-fuchsia-400'/>
+                      <span className='font-semibold'>Minha Marca</span>
+                    </div>
+                    <button className='p-2 rounded-lg' onClick={() => setOpen(false)}>
+                      <X className='size-5'/>
+                    </button>
+                  </div>
+                  <div className='flex flex-col gap-4 bg-slate-900 p-4 w-90'>
+                    {navLinks.map((link) => (
+                      <a key={link.href} href={link.href} className='text-slate-200' onClick={() => setOpen(false)}>
+                        {link.label}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+            </div>
           </div>
         )}
+        
       </header>
     </div>
   )
