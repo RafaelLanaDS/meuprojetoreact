587e7217c528d4487711ceae9813b82ddd0b5a46
Rafael Lana <134884057+RafaelLanaDS@users.noreply.github.com>
2026-09-09 19:29:09 -0300

    foi adicionado a pagina depoimentos

--- src/App.jsx
+++ src/App.jsx
@@ -175,7 +175,27 @@ function App() {
           ))}
         </div>
       </section>
-
+      {/* Depoimentos */}
+      <section id='testmonials' className='mx-auto max-w-6xl px-4 py-8'>
+          <h2 className='text-3xl font-bold'>Quem usou, aprovou</h2>
+          <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-4">
+            {[1, 2, 3].map((i) => 
+              <blockquote key={i} className='rounded-2xl border bolder-white/10 p-6'>
+                <div className="flex items-center gap-2 text-amber-400">
+                  {Array.from({ length: 5 }).map((_, i) =>
+                    <Star key={i} className='size-4 fill-current' />
+                  )}
+                </div>
+                <p className='mt-3 text-slate-300'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nobis et porro deserunt perspiciatis. Quo harum dolor sequi, iusto debitis cum nam sint tempora? 
+                </p>
+                <footer className='mt-3 text-sm-text-slate-400'>
+                  - Nome do cliente 
+                </footer>
+              </blockquote>
+            )}
+          </div>
+      </section>
     </div>
   )
 }
